96ea288174512ebd062b9ab830c9d2b62e08cb09
vas0x59 <vasily.yuryev.a1@gmail.com>
2020-05-10 01:01:38 +0700

    UPDATE

---
+++ Rover_Test_NRF/src/main.cpp
@@ -0,0 +1,11 @@
+#include <Arduino.h>
+#include <SPI.h>
+#include "RF24.h"
+
+void setup() {
+  // put your setup code here, to run once:
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+}
--- Sender_NRF/src/main.cpp
+++ Sender_NRF/src/main.cpp
@@ -77,6 +77,7 @@ void setup()
     pinMode(SENDING_PIN, OUTPUT);
     pinMode(ECHO1_PIN, INPUT);
     Serial.begin(115200);
+    setup_nrf();
 }
 
 void loop()

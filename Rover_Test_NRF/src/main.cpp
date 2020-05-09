bbaa5e095bf165f3384a2654fc0871ed4a12b776
vas0x59 <vasily.yuryev.a1@gmail.com>
2020-05-10 01:02:16 +0700

    Add nrf

--- Rover_Test_NRF/src/main.cpp
+++ Rover_Test_NRF/src/main.cpp
@@ -2,6 +2,26 @@
 #include <SPI.h>
 #include "RF24.h"
 
+#define NRF_CE A4
+#define NRF_CSN A5
+
+#define NRF_CHANEL 42
+// const uint64_t pipes[] = {0xF0F1F2F3F4L, };
+byte addresses[][6] = {"ping","back"};
+RF24 radio(NRF_CE, NRF_CSN);
+
+void setup_nrf()
+{
+    radio.begin();
+    delay(2);
+    radio.setChannel(NRF_CHANEL);
+    radio.setDataRate(RF24_1MBPS); 
+    radio.setPALevel(RF24_PA_HIGH); 
+    radio.openWritingPipe(addresses[1]);
+    radio.openReadingPipe(1, addresses[0]);
+    
+}
+
 void setup() {
   // put your setup code here, to run once:
 }
